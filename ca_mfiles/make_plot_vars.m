8bb1a859ea1f5ec91597b3c18d37b97b02faff85
Michele <imf.resdm@gmail.com>
2014-11-14 23:37:01 +0000

    resize variables in term of gdp

--- ca_mfiles/make_plot_vars.m
+++ ca_mfiles/make_plot_vars.m
@@ -1,16 +1,16 @@
 plot_vars = struct();
 
 plot_vars.y = y;
-plot_vars.ch = ch;
-plot_vars.k = k;
-plot_vars.s = s;
-plot_vars.f = f;
-plot_vars.ynon = ynon;
-plot_vars.invk = invk;
-plot_vars.invs = invs;
+plot_vars.ch = ch./y;
+plot_vars.k = k./y;
+plot_vars.s = s./y;
+plot_vars.f = f./y;
+plot_vars.ynon = ynon./y;
+plot_vars.invk = invk./y;
+plot_vars.invs = invs./y;
 plot_vars.r = r;
-plot_vars.tb = tb;
-plot_vars.ca = ca;
-plot_vars.govh = govh;
+plot_vars.tb = tb./y;
+plot_vars.ca = ca./y;
+plot_vars.govh = govh./y;
 
 save plot_vars.mat plot_vars
