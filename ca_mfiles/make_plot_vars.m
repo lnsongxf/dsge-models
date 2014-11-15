cb214a7144d3d9357658534c73597278f4cbdb8f
Michele <imf.resdm@gmail.com>
2014-11-15 00:24:47 +0000

    New App Order

--- ca_mfiles/make_plot_vars.m
+++ ca_mfiles/make_plot_vars.m
@@ -1,17 +1,17 @@
 plot_vars = struct();
 
 plot_vars.y = y;
+plot_vars.yoil=yoil./y*100;
+plot_vars.ynon=ynon./y*100;
+plot_vars.ca=ca./y*100;
+plot_vars.tb=tb./y*100;
+plot_vars.r=r./y*100;
+plot_vars.invk=invk./y*100;
+plot_vars.invs=invs./y*100;
 plot_vars.ch = ch./y*100;
 plot_vars.k = k./y*100;
 plot_vars.s = s./y*100;
 plot_vars.f = f./y*100;
-plot_vars.ynon = ynon./y*100;
-plot_vars.yoil = yoil./y*100;
-plot_vars.invk = invk./y*100;
-plot_vars.invs = invs./y*100;
-plot_vars.r = r*100;
-plot_vars.tb = tb./y*100;
-plot_vars.ca = ca./y*100;
 plot_vars.govh = govh./y*100;
 
 save plot_vars.mat plot_vars
